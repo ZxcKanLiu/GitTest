2af16e9b45bf4d04e60dc7bfe429655cd661c2d4
admin <2651268280@qq.com>
2022-06-15 14:34:07 +0800

    commit by fix_out

--- src/main/java/com/computer/controller/TestController.java
+++ src/main/java/com/computer/controller/TestController.java
@@ -4,5 +4,6 @@ public class TestController {
     public static void main(String[] args) {
         System.out.println("刘侃");
         System.out.println("二哈");
+        System.out.println("拉布拉多");
     }
 }
